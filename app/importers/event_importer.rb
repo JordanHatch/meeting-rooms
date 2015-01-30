ca3f21161c669c318efb84edccdda0a77b39ce3d
Jordan Hatch <jordan@jordanh.net>
2015-01-30 21:23:13 +0000

    Make the EventImporter support a room_id
    This changes the EventImporter so that it always takes an argument to
    specify which room events should be imported to.

--- app/importers/event_importer.rb
+++ app/importers/event_importer.rb
@@ -1,18 +1,19 @@
 class EventImporter
-  def initialize(calendar_id:)
+  def initialize(calendar_id:, room_id:)
     @calendar_id = calendar_id
+    @room_id = room_id
   end
 
   def import
     events.each do |response|
       event = ApiFormattedEvent.build_from_response(response).to_event_instance
-      event.room_id = 1
+      event.room_id = room_id
       event.save
     end
   end
 
 private
-  attr_reader :calendar_id
+  attr_reader :calendar_id, :room_id
 
   def events
     calendar_api.events(calendar_id: calendar_id)
--- spec/importers/event_importer_spec.rb
+++ spec/importers/event_importer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe EventImporter do
 
-  let(:calendar_id) { 'foo' }
+  let(:room) { create(:room) }
   let(:mock_calendar_api) { double(:calendar_api) }
   let(:events) {
     [
@@ -44,12 +44,12 @@ RSpec.describe EventImporter do
   describe '#import' do
     it 'creates Event objects for each event returned from the Calendar API' do
       expect(mock_calendar_api).to receive(:events).with(
-        hash_including(calendar_id: calendar_id)
+        hash_including(calendar_id: room.calendar_id)
       ).and_return(events)
 
-      importer = EventImporter.new(calendar_id: calendar_id)
+      importer = EventImporter.new(calendar_id: room.calendar_id, room_id: room.id)
 
-      expect{ importer.import }.to change{ Event.count }.by(2)
+      expect{ importer.import }.to change{ room.events.count }.by(2)
     end
   end
 
