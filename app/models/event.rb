c2e13ddc24bff00cd77c56d7f594cf0687a3f103
Jordan Hatch <jordan@jordanh.net>
2015-01-31 12:28:53 +0000

    Add scope to Event to return current events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ActiveRecord::Base
   belongs_to :room
+
+  scope :current, -> {
+    where(['start_at < ? AND end_at > ?', Time.now, Time.now])
+  }
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Event, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '.current' do
+    before do
+      Timecop.freeze
+    end
+
+    it 'returns events where start_at is before now and end_at is after now' do
+      included_event = create(:event, start_at: 30.minutes.ago,
+                                      end_at: 10.minutes.from_now)
+      other_events = [
+        create(:event, start_at: 30.minutes.from_now),
+        create(:event, start_at: 30.minutes.ago, end_at: 1.minute.ago)
+      ]
+
+      events = Event.current
+
+      expect(events.size).to eq(1)
+      expect(events.first).to eq(included_event)
+    end
+  end
+
 end
