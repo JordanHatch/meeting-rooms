dd01d01b22186d7fb466d8d3c026fb4b0d478576
Jordan Hatch <jordan@jordanh.net>
2015-01-31 12:33:03 +0000

    Return rooms which are either in use or not in use
    This adds scopes to Room to allow filtering of rooms by whether they 
    have any current events or not.
    
    This also adds a new method '#in_use?' to an instance of Room, which 
    returns true if the particular room has any current events.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,13 @@
 class Room < ActiveRecord::Base
   has_many :events
+  has_many :current_events, -> { current }, class_name: 'Event'
 
   validates :title, :short_title, :calendar_id, presence: true
+
+  scope :in_use, -> { joins(:current_events) }
+  scope :not_in_use, -> { where.not(id: in_use) }
+
+  def in_use?
+    current_events.any?
+  end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Room, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe '.in_use' do
+    it 'returns rooms with current events' do
+      rooms = create_list(:room, 2)
+
+      # create a current event for the first room
+      # so it can be expected to be 'in use'
+      create(:current_event, room: rooms[0])
+
+      # only create a past event for the second room
+      # so we won't expect it to be 'in use'
+      create(:past_event, room: rooms[1])
+
+      result = Room.in_use
+
+      expect(result.size).to eq(1)
+      expect(result.first).to eq(rooms[0])
+    end
+  end
+
+  describe '.not_in_use' do
+    it 'returns rooms with no current events' do
+      rooms = create_list(:room, 2)
+
+      create(:current_event, room: rooms[0])
+      create(:past_event, room: rooms[1])
+
+      result = Room.not_in_use
+      expect(result.size).to eq(1)
+      expect(result.first).to eq(rooms[1])
+    end
+  end
+
+  describe '#in_use?' do
+    let(:room) { create(:room) }
+
+    it 'returns true if a room has events happening now' do
+      create(:current_event, room: room)
+
+      expect(room).to be_in_use
+    end
+
+    it 'returns false if a room has no events happening now' do
+      create(:past_event, room: room)
+      create(:future_event, room: room)
+
+      expect(room).to_not be_in_use
+    end
+  end
 end
