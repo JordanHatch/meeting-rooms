5bd3bf9518b2c61ce794f3aa6d928f973b59f4e6
Jordan Hatch <jordan@jordanh.net>
2015-02-24 23:59:38 +0000

    Tidy up and add a TODO

--- app/presenters/room_presenter.rb
+++ app/presenters/room_presenter.rb
@@ -13,6 +13,11 @@ class RoomPresenter < SimpleDelegator
     list
   end
 
+  # TODO: can this be simplified to:
+  #
+  # - return both formatted and full timestamps in a nested object
+  # - DRY up with the JSON representation
+  #
   def as_mustache_context
     {
       schedule: events_with_gaps.map {|event|
